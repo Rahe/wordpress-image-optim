2174279b5a16f253395974d0521b38b1fefe4312
Nicolas Juen <njuen@beapi.fr>
2016-06-12 17:53:34 +0200

    Add gif support

--- imageoptim.js
+++ imageoptim.js
@@ -1,16 +1,16 @@
 var imagemin = require('imagemin');
 var imageminMozjpeg = require('imagemin-mozjpeg');
 var imageminPngquant = require('imagemin-pngquant');
-
+var imageminGifsicle = require('imagemin-gifsicle');
 
 imagemin([process.argv[2]],
 	process.argv[3],
 	{
 		plugins: [
 			imageminMozjpeg({quality: 70}),
-			imageminPngquant({quality: 70})
+			imageminPngquant({quality: 70}),
+			imageminGifsicle({optimizationLevel: 3}),
 		]
-	}).catch(error = > {
+	}).catch(error => {
 	console.error(error);
-} )
-;
+} );
